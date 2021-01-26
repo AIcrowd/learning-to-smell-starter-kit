15ecdb3dc15f246b0cd9404d7feff2257331dc84
yacotaco <kamilkurach@protonmail.com>
2021-01-27 00:33:20 +0100

    Remove test imports and prints

--- predict.py
+++ predict.py
@@ -4,9 +4,6 @@ Import implementations you want to play around with here
 from random_predict import RandomPredictor
 from fingerprint_predict import FingerprintPredictor
 from java_random_predict import JavaRandomPredictor
-import rdkit
-import pandas
-import numpy
 
 """
 The implementation you want to submit as your submission
@@ -14,9 +11,7 @@ The implementation you want to submit as your submission
 # submission = FingerprintPredictor() # This is a class derived from the L2SPredictor class from evaluator.learning_to_smell
 submission = RandomPredictor() # This is a class derived from the L2SPredictor class from evaluator.learning_to_smell
 print("Generating prediction using %s" % submission.__class__.__name__)
-print(rdkit)
-print(pandas)
-print(numpy)
 
 submission.run()
 print("Successfully generated prediction file at %s" % submission.predictions_output_path)
+
