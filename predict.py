da90616618d588f3f9e67300b453a87d140d25db
yacotaco <kamilkurach@protonmail.com>
2021-01-29 07:01:56 +0100

    Add: my model to predict

--- predict.py
+++ predict.py
@@ -4,12 +4,14 @@ Import implementations you want to play around with here
 from random_predict import RandomPredictor
 from fingerprint_predict import FingerprintPredictor
 from java_random_predict import JavaRandomPredictor
+from my_model import MyModel
 
 """
 The implementation you want to submit as your submission
 """
 # submission = FingerprintPredictor() # This is a class derived from the L2SPredictor class from evaluator.learning_to_smell
-submission = RandomPredictor() # This is a class derived from the L2SPredictor class from evaluator.learning_to_smell
+# submission = RandomPredictor() # This is a class derived from the L2SPredictor class from evaluator.learning_to_smell
+submission = MyModel()
 print("Generating prediction using %s" % submission.__class__.__name__)
 
 submission.run()
